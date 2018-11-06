bd91230c91b49e871f194debbe202a059c894d0a
10-m <efurukaw@ncc.go.jp>
2018-11-06 14:15:00 +0900

    Add box, kde, violin plots

---
+++ python/seaborn/boxplot.py
@@ -0,0 +1,26 @@
+#!env python3
+# -*- coding: utf-8 -*-
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+n = 10000
+a = np.random.normal(40, 30, size=n)
+b = np.random.normal(60, 20, size=n)
+c = np.random.normal(80, 10, size=n)
+
+df = pd.DataFrame({
+    "label": ["a"] * n + ["b"] * n + ["c"] * n,
+    "value": np.concatenate([a, b, c])
+})
+
+sns.set_style("darkgrid")
+sns.set_palette("pastel")
+
+sns.boxplot(data=df, x="label", y="value")
+
+plt.savefig('boxplot.png')
+plt.close('all')
