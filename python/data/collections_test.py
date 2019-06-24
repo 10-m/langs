9cd1fe20ce9b866c131b5dcb6f16ca1adacfffb0
10-m <efurukaw@ncc.go.jp>
2019-06-24 20:36:36 +0900

    Modify collection module

--- python/data/collections_test.py
+++ python/data/collections_test.py
@@ -1,9 +1,17 @@
 #!env python3
 # -*- coding: utf-8 -*-
-from collections import Counter
+from collections import Counter, defaultdict
 
 # Counter
 count = Counter(["apple", "melon", "orange", "apple", "orange", "apple"])
 print(count)
 print(count.most_common(2))
 print(count.most_common(1))
+
+# defaultdict
+people = ((20,"Taro"),(23,"Ken"),(21,"Rin"),(20,"Joe"),(22,"Risa"))
+d = defaultdict(list)
+for age, name in people:
+    d[age].append(name)
+print(d)
+
