d308b35b4588fe5c3a9bec561704b85120d1da8b
10-m <efurukaw@ncc.go.jp>
2020-02-10 20:43:27 +0900

    Add number.py

---
+++ python/data/number.py
@@ -0,0 +1,24 @@
+#!env python3
+# -*- coding: utf-8 -*-
+
+# convert
+print(int(1.7))
+print(float(2))
+print(int('1'))
+print(float('1.2'))
+
+numbers = [10, 3, 22, 42, 145]
+print(abs(-1))
+print(abs(-5.0))
+print(abs(255))
+print(max(numbers))
+print(min(numbers))
+
+# divmod
+x1, y1 =divmod(10, 3)
+print('商{}, 余り{}'.format(x1, y1))
+
+# type
+print(type(4))
+print(type(2.5))
+print(type('abc')) 
