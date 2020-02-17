b3ce28150f2aa05c8a305a182910bae1012c6921
10-m <efurukaw@ncc.go.jp>
2020-02-17 19:50:57 +0900

    Add example

--- python/string/format.py
+++ python/string/format.py
@@ -8,3 +8,14 @@ print('{0}, {1}, {0}!'.format('Hello', 'World'))
 
 # name
 print('{name} {age}'.format(name='aaa', age=123))
+
+# number
+x = 11
+y = 2.5555555
+z = 1000000
+print('{:d}'.format(x))
+print('{:5d}'.format(x)) # 右詰め
+print('{:05d}'.format(x)) # ゼロ詰め
+print('{:f}'.format(y))
+print('{:.2f}'.format(y)) 
+print('{:,}'.format(z))# 1,000,000
