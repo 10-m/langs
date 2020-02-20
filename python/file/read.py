802e1e8089375fd37bdb6ee5f3bcb8c6a9caf4b3
10-m <efurukaw@ncc.go.jp>
2020-02-20 19:59:56 +0900

    Merge file

--- python/file/read.py
+++
@@ -1,9 +0,0 @@
-#!env python3
-# -*- coding: utf-8 -*-
-import re
-
-file = 'read.py';
-with open(file, 'r') as fd:
-    for line in fd:
-        line = re.sub('\r?\n?$', '', line)
-        print(line)
