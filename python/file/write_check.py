34fae84e5ae7e8ad6a157d322406ca3e37e75526
10-m <efurukaw@ncc.go.jp>
2020-02-20 20:00:22 +0900

    Rename file

--- python/file/write_check.py
+++
@@ -1,9 +0,0 @@
-#!env python3
-# -*- coding: utf-8 -*-
-
-file = 'tmp.txt'
-try:
-    with open(file, 'x') as fd:
-        fd.write('Hello' + '\n')
-except FileExistsError:
-    print(file + ' exits already')
