7975bcc87d5aaf81be74f7090b9c3de678e94479
10-m <efurukaw@ncc.go.jp>
2020-03-02 17:40:09 +0900

    Update example

--- python/data/set.py
+++ python/data/set.py
@@ -1,15 +1,29 @@
 #!env python3
 # -*- coding: utf-8 -*-
 
+## initalize
+
 s1 = {"dog", "cat", "pig", "cow"}
 print(s1)
 
+# from list
 s2 = set(["dog", "cat", "pig", "cow"])
 print(s1)
 
+# from tuple
+s3 = set(('A', 'B', 'C', 'C'))
+print(s3)
+
+# empty
+print(set())
+
+## method
 # add
 s1 = {"red", "green", "blue"}
 s1.add("black")
+
+# add tuple
+s1.add(('white', 'purple'))
 print(s1)
 
 # update
@@ -17,22 +31,12 @@ s1 = {"red", "green", "blue"}
 s1.update(["black", "yellow"])
 print(s1)
 
-# operator
-print({1, 4, 5} & { 4, 3, 20}) # s1.intersection(s2)
-print({1, 4, 5} | { 4, 3, 20}) # s1.union(s2)
-print({1, 4, 5} - { 4, 3, 20}) # s1.difference(s2)
-print({1, 4, 5} ^ { 4, 3, 20}) # s1.symmetric_difference(s2)
-print({1, 2} <= {1, 2})
-print({1, 2} <= {1, 2, 3})
-print({4, 5, 6} > {4, 5, 6})
-print({4, 5, 6} > {4, 5})
-
-# remove
+# remove。指定した要素が無いときはKeyError
 s1 = {"blue", "black", "red", "green"} 
 s1.remove("blue")
 print(s1)
 
-# discard
+# discard。指定した要素が無いときはKeyErrorにならない
 s1 = {"blue", "black", "red", "green"} 
 s1.discard("blue")
 print(s1)
@@ -41,11 +45,8 @@ print(s1)
 
 # pop
 myset = {1965, 1959, 2001, 1987, 1959, 2011}
-while True:
-    try:
-        print(myset.pop())
-    except:
-        break
+while myset:
+    print(myset.pop())
 
 # frozenset
 s1 = {"green", "blue"} 
@@ -59,3 +60,13 @@ s2 = s1.copy()
 s1.add("大阪府")
 print(s1)
 print(s2)
+
+## operator
+print({1, 4, 5} & { 4, 3, 20}) # s1.intersection(s2)
+print({1, 4, 5} | { 4, 3, 20}) # s1.union(s2)
+print({1, 4, 5} - { 4, 3, 20}) # s1.difference(s2)
+print({1, 4, 5} ^ { 4, 3, 20}) # s1.symmetric_difference(s2)
+print({1, 2} <= {1, 2}) # 左辺の要素全てが右辺の集合に含まれているか
+print({1, 2} <= {1, 2, 3})
+print({4, 5, 6} > {4, 5, 6})# 左辺は右辺の部分集合か (完全一致は除く)
+print({4, 5, 6} > {4, 5})
