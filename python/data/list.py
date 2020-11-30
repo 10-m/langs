4c789c44c2886a74dfaf7328bbf6b206b7b0c812
10-m <efurukaw@ncc.go.jp>
2020-11-30 19:12:28 +0900

    Add examples to list.py

--- python/data/list.py
+++ python/data/list.py
@@ -9,6 +9,7 @@ print('lst[-1]:', lst[-1])
 # slice
 print('lst[1:3]:', lst[1:3])
 print('lst[1:]:', lst[1:])
+print('lst[:2]:', lst[:2])
 print('lst[1::2]:', lst[1::2])
 print('lst[:0:-1]:', lst[:0:-1])
 
@@ -24,3 +25,18 @@ lst[1:4] = 20, 30
 print('lst:', lst)
 lst.append(40)
 print('lst:', lst)
+
+# insert
+lst = [1, 2, 3, 4]
+lst.insert(1, 100)
+print('lst:', lst)
+
+# pop
+numbers = [100, 200, 300, 400, 500]
+# 末尾を削除
+print(numbers.pop())
+print(numbers)
+
+# 2番目の要素の削除
+numbers.pop(1)
+print(numbers)
