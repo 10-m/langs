ded540b8a7e27abacf4aec1b606975aaa02e5085
10-m <efurukaw@ncc.go.jp>
2021-01-20 18:52:52 +0900

    Add example

--- python/www/flask/template_dict.py
+++ python/www/flask/template_dict.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     duties = {'月': '佐藤', '火': '鈴木', '水': '清水', '木': '中村', '金': '高橋'}
-    return render_template('dict.html', duties=duties)
+    dict1 = {'age': 24}
+    empty = {}
+    return render_template('dict.html', duties=duties, dict1=dict1, empty=empty)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
